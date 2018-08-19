dc80b4bd941e6b73f05b92b3c852198706d4c889
kevinglinski <kevin.glinski@inin.com>
2018-08-18 21:13:26 -0400

    sys.exit for any exceptions

--- cfn_runner/__main__.py
+++ cfn_runner/__main__.py
@@ -41,73 +41,89 @@ def main():
     parser.add_argument('--resources', dest='resources_directory',
                     help='Path to the resources directory', required=True)                   
 
+    try:
+        args = parser.parse_args()
 
-    args = parser.parse_args()
+        stack_properties = None
+        stack_tags = {}
 
-    stack_properties = None
-    stack_tags = None
+        if not args.properties_filename:
+            print("properties filename not defined")
+            sys.exit(1)
 
-    if not args.properties_filename:
-        print("properties filename not defined")
-        sys.exit(1)
+        with open(args.properties_filename, 'r') as stream:
+            try:
+                stack_properties = yaml.load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
 
-    with open(args.properties_filename, 'r') as stream:
-        try:
-            stack_properties = yaml.load(stream)
-        except yaml.YAMLError as exc:
-            print(exc)
-
-    with open(args.tags_filename, 'r') as stream:
-        try:
-            stack_tags = yaml.load(stream)
-        except yaml.YAMLError as exc:
-            print(exc)        
+        if args.tags_filename:
+            with open(args.tags_filename, 'r') as stream:
+                try:
+                    stack_tags = yaml.load(stream)
+                except yaml.YAMLError as exc:
+                    print(exc)   
+                    sys.exit(1)     
 
 
-    taglist = []
-    for key in stack_tags:
-        prop = {
-            "Key": key,
-            "Value": stack_tags[key]
-        }
+        taglist = []
+        for key in stack_tags:
+            prop = {
+                "Key": key,
+                "Value": stack_tags[key]
+            }
 
-        taglist.append(prop)
+            taglist.append(prop)
 
-    parameter_list = []
-    print(stack_properties)
+        parameter_list = []
+        print(stack_properties)
 
-    cloudformation = boto3.client('cloudformation', region_name=stack_properties['region'])
+        cloudformation = boto3.client('cloudformation', region_name=stack_properties['region'])
 
-    for propkey in stack_properties['parameters']:
-        value = stack_properties['parameters'][propkey]
-        print(value)
-        if value[0] is "$":
-            value = os.environ[value[1:]]
+        for propkey in stack_properties['parameters']:
+            value = stack_properties['parameters'][propkey]
+            print(value)
+            if value[0] is "$":
+                value = os.environ[value[1:]]
 
-        prop = {
-            "ParameterKey": propkey,
-            "ParameterValue": value
-        }
+            prop = {
+                "ParameterKey": propkey,
+                "ParameterValue": value
+            }
 
-        parameter_list.append(prop)    
+            parameter_list.append(prop)    
 
-    resources = {}
-    for file in os.listdir(args.resources_directory):
-        with open(args.resources_directory + "/" + file, 'r') as stream:
+        resources = {}
+        for file in os.listdir(args.resources_directory):
+            with open(args.resources_directory + "/" + file, 'r') as stream:
 
-            file_resources = yaml.load(stream)
-            print('-----')
-            print(file_resources)
-            resources = merge_dicts(resources, file_resources)
+                file_resources = yaml.load(stream)
+                print('-----')
+                print(file_resources)
+                resources = merge_dicts(resources, file_resources)
 
-    print(resources)
+        print(resources)
 
-    response = {}
+        response = {}
 
-    if has_stack(stack_properties['stackname']):
-        try:
+        if has_stack(stack_properties['stackname']):
+            try:
 
-            response = cloudformation.update_stack(
+                response = cloudformation.update_stack(
+                    StackName=stack_properties['stackname'],
+                    TemplateBody=json.dumps(resources),
+                    Tags=taglist,
+                    Capabilities=[
+                        'CAPABILITY_IAM',
+                    ],
+                    Parameters=parameter_list
+                )
+            except Exception as e:
+                if 'No updates are to be performed' not in str(e) :
+                    print ('not in e')
+                    raise e
+        else:
+            response = cloudformation.create_stack(
                 StackName=stack_properties['stackname'],
                 TemplateBody=json.dumps(resources),
                 Tags=taglist,
@@ -115,38 +131,25 @@ def main():
                     'CAPABILITY_IAM',
                 ],
                 Parameters=parameter_list
+                
             )
-        except Exception as e:
-            if 'No updates are to be performed' not in str(e) :
-                print ('not in e')
-                raise e
-    else:
-        response = cloudformation.create_stack(
-            StackName=stack_properties['stackname'],
-            TemplateBody=json.dumps(resources),
-            Tags=taglist,
-            Capabilities=[
-                'CAPABILITY_IAM',
-            ],
-            Parameters=parameter_list
-            
-        )
 
-    if response:
-        print(response)
+        if response:
+            print(response)
 
-        stack_status = get_stack_status(stack_properties['stackname'], stack_properties)
-        while "COMPLETE" not in stack_status:
-            print (stack_status)
-            time.sleep(3)
             stack_status = get_stack_status(stack_properties['stackname'], stack_properties)
+            while "COMPLETE" not in stack_status:
+                print (stack_status)
+                time.sleep(3)
+                stack_status = get_stack_status(stack_properties['stackname'], stack_properties)
 
-        print (stack_status)
-        if "ROLLBACK" in stack_status:
-            raise "Stack not updated properly"
-    #     UPDATE_IN_PROGRESS
-    else:
-        print ("nothing to do")
-
+            print (stack_status)
+            if "ROLLBACK" in stack_status:
+                raise "Stack not updated properly"
+        #     UPDATE_IN_PROGRESS
+        else:
+            print ("nothing to do")
+    except :
+        sys.exit(1)     
 if __name__ == '__main__':
     main()
