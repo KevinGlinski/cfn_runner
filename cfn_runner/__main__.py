bd151155f908c3a270baa368a0f54c80979a39c7
Kevin Glinski <KevinGlinski@users.noreply.github.com>
2020-03-18 11:34:58 -0400

    adding  CAPABILITY_NAMED_IAM

--- cfn_runner/__main__.py
+++ cfn_runner/__main__.py
@@ -207,6 +207,7 @@ def main():
                 Tags=taglist,
                 Capabilities=[
                     'CAPABILITY_IAM',
+                    'CAPABILITY_NAMED_IAM'
                 ],
                 Parameters=parameter_list
                 
