901941c75e74e69590a57fe10442b5d3430c8e6a
Kevin <kevin.glinski@mypureinsights.com>
2025-03-12 12:28:49 -0400

    set deepmerge ver

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 setup(
     name = 'cfn_runner',
-    version = '0.1.0',
+    version = '0.2.0',
     packages = ['cfn_runner'],
     license='mit',
-    install_requires = ['boto3','pyyaml', 'deepmerge'],
+    install_requires = ['boto3','pyyaml', 'deepmerge-2.0'],
     entry_points = {
         'console_scripts': [
             'cfn_runner = cfn_runner.__main__:main'
